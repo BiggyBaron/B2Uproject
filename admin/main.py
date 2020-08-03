cf61392124203d9ab0ac0afcff265e785fd1ba0d
naboox <dr.cleverest@gmail.com>
2020-08-03 16:44:18 +0600

    Added more shit

--- admin/main.py
+++ admin/main.py
@@ -87,7 +87,7 @@ def tubes_calc():
                     new_dates.append(time)
                     new_values.append(date["data"])
                     times.append(date["time"])
-                    values.append([times[-1], new_values[-1]-new_values[-2]])
+                    values.append([times[-1], (int(new_values[-1])-int(new_values[-2]))])
         
         period = datetime.datetime.fromtimestamp(times[0]) - datetime.datetime(2020, 7, 30, 0, 0, 0)
         average = round(float(new_values[0])/period.days)
