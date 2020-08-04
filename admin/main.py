39b6144b92403b056ae4d192db3e756452738566
naboox <dr.cleverest@gmail.com>
2020-08-04 12:58:11 +0600

    seconds to milliseconds

--- admin/main.py
+++ admin/main.py
@@ -92,9 +92,9 @@ def tubes_calc():
 
         for i in range(len(new_values)):
             if i>0:
-                values.append([ datetime.datetime.timestamp(datetime.datetime.strptime(new_dates[i], "%d.%m.%y")) , int(new_values[i]) - int(new_values[i-1])])
+                values.append([ datetime.datetime.timestamp(datetime.datetime.strptime(new_dates[i]*1000, "%d.%m.%y")) , int(new_values[i]) - int(new_values[i-1])])
             else:
-                values.append([ datetime.datetime.timestamp(datetime.datetime.strptime(new_dates[i], "%d.%m.%y")) , int(new_values[i])])
+                values.append([ datetime.datetime.timestamp(datetime.datetime.strptime(new_dates[i]*1000, "%d.%m.%y")) , int(new_values[i])])
         
         period = datetime.datetime.fromtimestamp(times[-1]) - datetime.datetime(2020, 7, 28, 0, 0, 0)
         average = round(float(new_values[-1])/period.days)
@@ -119,11 +119,11 @@ def tubes_calc():
         total = 0
         for obj in objects:
             for i in range(len(new_tubes[obj]["values"])):
-                if new_tubes[obj]["values"][i][0] == datetime.datetime.timestamp(today):
+                if new_tubes[obj]["values"][i][0] == datetime.datetime.timestamp(today)*1000:
                     total = total + new_tubes[obj]["values"][i][1]
             
             
-        new_tubes["total"]["values"].append([datetime.datetime.timestamp(today), total])
+        new_tubes["total"]["values"].append([datetime.datetime.timestamp(today)*1000, total])
     
     # logging.warning(new_tubes["total"]["values"])
 
